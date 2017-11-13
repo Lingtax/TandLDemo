2293d8bc880f47fe878acb7d9a2f2d17b3a8b012
Mathew Ling <m.ling@deakin.edu.au>
2017-11-13 22:57:55 +1100

    Adds titles to plots, tidies server script

--- server.R
+++ server.R
@@ -2,10 +2,9 @@ library(shiny)
 library(gsheet)
 library(tidyverse)
 
-# Define server logic required to draw a histogram
 shinyServer(function(input, output) {
     
-    df <-  gsheet2tbl('docs.google.com/spreadsheets/d/1tk5bX8e0OfcYcB6qndiVZ2X46m9O0Cy1H2pDRrlRoNY') %>% 
+    df <-  gsheet2tbl("docs.google.com/spreadsheets/d/1tk5bX8e0OfcYcB6qndiVZ2X46m9O0Cy1H2pDRrlRoNY") %>% 
     rename(Faculty = `What Faculty are you from?`,
            GroupsTaught = `Which groups do you teach?`,
            DataPerson   = `Do you self identify as a Research Methods, Analyst, or Data science person?`,
@@ -21,36 +20,27 @@ shinyServer(function(input, output) {
               DataPerson	 = as.factor(DataPerson),
               Replicable   = Replicable*10
             )
-##### work from here, rates by faculty, difference between data people and non-data people relationship between knowledge and Replicability
 
-      # knowledge ~ faculty
-      
-      # Replicability ~ faculty
-      
-      # Data person ~ DFrate
-      
-      # Corr knowledge and rDFbad
-      # corr rDFBad and Replicable
   # differences
   output$knowFac <- renderPlot(ggplot(df, aes(Faculty, OpenKnow, colour = Faculty, group = Faculty)) + 
-                                  geom_violin() + geom_jitter(alpha=.5) + theme_classic()
+                                  geom_violin() + geom_jitter(alpha=.5) + theme_classic() + ggtitle("Open science knowledge by Faculty")
                                 )
   output$replFac <- renderPlot(ggplot(df, aes(Faculty, Replicable, colour = Faculty, group = Faculty)) + 
-                                 geom_violin() + geom_jitter(alpha=.5) + theme_classic()
+                                 geom_violin() + geom_jitter(alpha=.5) + theme_classic() + ggtitle("Estimated Replicability of field by Faculty")
   )
   
   output$DPDFrate <- renderPlot(ggplot(df, aes(DataPerson, DFrate, colour = DataPerson, group = DataPerson)) + 
-                                 geom_violin() + geom_jitter(alpha=.5) + theme_classic()
+                                 geom_violin() + geom_jitter(alpha=.5) + theme_classic() + ggtitle("Estimated rate of Researcher dfs by identification as 'data person'" )
   )
   
   # relationships
   output$knowbad <- renderPlot(ggplot(df, aes(OpenKnow, DFbad)) + 
                                  geom_point() + geom_smooth(method="lm", se = FALSE) +  
-                                 theme_classic()
+                                 theme_classic() + ggtitle("Relationship between knowledge of Open science and Concern with researcher dfs")
                                )
   output$BadRepl <- renderPlot(ggplot(df, aes(DFbad, Replicable)) + 
                                  geom_point() + geom_smooth(method="lm", se = FALSE) +  
-                                 theme_classic()
+                                 theme_classic() + ggtitle("Relationship between Concern with researcher dfs and estimated replicability of field")
   )  
      
 })
