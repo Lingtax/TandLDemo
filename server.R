dc165817894113150465ec0d2e06cc97d18cd29b
Mathew Ling <m.ling@deakin.edu.au>
2017-11-13 19:10:33 +1100

    Simplified and implemented

--- server.R
+++ server.R
@@ -6,35 +6,51 @@ library(tidyverse)
 shinyServer(function(input, output) {
     
     df <-  gsheet2tbl('docs.google.com/spreadsheets/d/1tk5bX8e0OfcYcB6qndiVZ2X46m9O0Cy1H2pDRrlRoNY') %>% 
-      mutate(Timestamp = lubridate::mdy_hms(Timestamp),
+    rename(Faculty = `What Faculty are you from?`,
+           GroupsTaught = `Which groups do you teach?`,
+           DataPerson   = `Do you self identify as a Research Methods, Analyst, or Data science person?`,
+           OpenKnow = `How would you rate your knowledge of open sciences prior to this workshop?`,
+           Replicable = `What proportion of results in your field do you expect to be replicable / valid?`,
+           DFrate = `To what extent do you believe researchers in your field utilise undisclosed "Researcher Degrees of Freedom" to "improve their results" (e.g. biased recruitment, optional stopping, undisclosed outlier exclusions, Hypothesising after the results are known)?`,
+           DFbad = `How "bad" is using researcher degrees of freedom to improve your results?`) %>%  
+    mutate(Timestamp = lubridate::mdy_hms(Timestamp),
               Faculty	     = as.factor(Faculty),
               Teach_UG     = grepl("Undergraduates", GroupsTaught),
               Teach_PG     = grepl("Postgraduates (Coursework)", GroupsTaught),
               Teach_Res    = grepl("Research Students", GroupsTaught), 
               DataPerson	 = as.factor(DataPerson),
-              ReplicabilityField = ReplicabilityField*10
+              Replicable   = Replicable*10
             )
 ##### work from here, rates by faculty, difference between data people and non-data people relationship between knowledge and Replicability
 
+      # knowledge ~ faculty
+      
+      # Replicability ~ faculty
+      
+      # Data person ~ DFrate
+      
+      # Corr knowledge and rDFbad
+      # corr rDFBad and Replicable
   # differences
-  output$diffFac <- renderTable({as.data.frame(group_by_(df, input$diffCat) %>% 
-                                                   summarize(mean(!!rlang::sym(input$diffCon))))})
-  
-  output$diffPlot <- renderPlot(ggplot(df, aes_string(input$diffCat, input$diffCon, colour = input$diffCat, group = input$diffCat)) + 
+  output$knowFac <- renderPlot(ggplot(df, aes(Faculty, OpenKnow, colour = Faculty, group = Faculty)) + 
                                   geom_violin() + geom_jitter(alpha=.5) + theme_classic()
                                 )
+  output$replFac <- renderPlot(ggplot(df, aes(Faculty, Replicable, colour = Faculty, group = Faculty)) + 
+                                 geom_violin() + geom_jitter(alpha=.5) + theme_classic()
+  )
+  
+  output$DPDFrate <- renderPlot(ggplot(df, aes(DataPerson, DFrate, colour = DataPerson, group = DataPerson)) + 
+                                 geom_violin() + geom_jitter(alpha=.5) + theme_classic()
+  )
+  
   # relationships
-  subs <- reactive({select(df, input$rel1, input$rel2)})
-  output$rel <- renderText({
-    corr <-  cor(subs())
-    paste("The relationship has a correlation coefficient of", round(corr[1, 2], 2))
-    })
-  output$relPlot <- renderPlot(ggplot(subs(), aes_string(input$rel1, input$rel2)) + 
+  output$knowbad <- renderPlot(ggplot(df, aes(OpenKnow, DFbad)) + 
                                  geom_point() + geom_smooth(method="lm", se = FALSE) +  
                                  theme_classic()
                                )
-    
+  output$BadRepl <- renderPlot(ggplot(df, aes(DFbad, Replicable)) + 
+                                 geom_point() + geom_smooth(method="lm", se = FALSE) +  
+                                 theme_classic()
+  )  
      
-  output$table <- renderTable(head(df))
-  
 })
--- ui.R
+++ ui.R
@@ -6,44 +6,17 @@ shinyUI(fluidPage(
   # Application title
   titlePanel("Demonstration of Live Data interactions"),
   
-  # Sidebar 
-  sidebarLayout(
-    sidebarPanel(
-      h1("Group differences"),
-      selectInput("diffCat", "Difference between: ",
-                   choices = c("Faculty",
-                               "DataPerson"),
-                   selected = "Faculty"),
-      selectInput("diffCon", "Difference on: ",
-                   choices = c("OpenSciKnowledge",
-                               "ReplicabilityField",
-                               "ResDFRate"), 
-                   selected = "OpenSciKnowledge"),
+  
+  fluidRow(
+    column(3, plotOutput("knowFac")),
+    column(3, plotOutput("replFac")),
+    column(3, plotOutput("DPDFrate"))
+           ),
+  
       hr(),
-      h1("Relationships"),
-      selectInput("rel1", "Difference on: ",
-                choices = c("Languages",
-                            "Siblings",
-                            "Age",
-                            "heightmetres",
-                            "Anxious")),
-      selectInput("rel2", "Difference on: ",
-                  choices = c("Languages",
-                            "Siblings",
-                            "Age",
-                            "heightmetres",
-                            "Anxious"),
-                 selected = "Siblings")
-      ),
-      
-    
-    mainPanel(
-      fluidRow(tableOutput("diffTable"),
-              plotOutput("diffPlot")),
-      hr(),
-      fluidRow(textOutput("rel"),
-              plotOutput("relPlot"))
-                       
+  fluidRow(
+    column(6, plotOutput("knowbad")),
+    column(6, plotOutput("BadRepl"))
     )
   )
-))
+)
